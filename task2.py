125f6c0fd4495fbbedddc7a20aea113f105df5f7
S-ABHISHEK-1905 <abhidhanaml@gmail.com>
2025-11-15 21:29:39 +0530

    Task 2 except Chunking

--- task2.py
+++ task2.py
@@ -1,10 +1,10 @@
 from docling.document_converter import DocumentConverter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains import RetrievalQA
-from langchain_core.vectorstores import VectorStoreRetriever
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 
-source = "./t3.pdf"  # document per local path or URL
+
+
+source = "./task2_data.pdf"  # document per local path or URL
 converter = DocumentConverter()
 result = converter.convert(source)
 text=result.document.export_to_markdown()
@@ -17,6 +17,13 @@ EMBEDDING_MODEL = "intfloat/e5-small-v2"
 embedding_model = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL)
 vector_store = FAISS.from_texts(texts=sentences, embedding=embedding_model)
 
-llm="deepseek-r1:8b"
-retriever = VectorStoreRetriever(vector_store)
-retrievalQA = RetrievalQA.from_llm(llm=llm, retriever=retriever)
+query = "What is langchain?"
+
+top_k = 3
+results = vector_store.similarity_search_with_score(query, k=top_k)
+
+retrieved_chunks = [doc[0].page_content for doc in results]
+scores = [float(doc[1]) for doc in results]
+
+output = {"chunks": retrieved_chunks,"scores": scores}
+print(output)
