3863bbb1881e14212f721635bce68fc9000d8546
S-ABHISHEK-1905 <abhidhanaml@gmail.com>
2025-11-15 21:42:45 +0530

    TASK 2 FINISHED

--- task2.py
+++ task2.py
@@ -1,4 +1,5 @@
 from docling.document_converter import DocumentConverter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
@@ -8,14 +9,22 @@ source = "./task2_data.pdf"
 converter = DocumentConverter()
 result = converter.convert(source)
 text=result.document.export_to_markdown()
-sentences = []
+
+chunks = []
+
+#We use a chunk size of 300 because the pdf contains statements of short facts about Langchain and Rag
+
 if text:
-  lines = [line.strip() for line in text.split('.') if line.strip()]
-  sentences.extend(lines)
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=300,        
+        chunk_overlap=50       
+    )
+    chunks = splitter.split_text(text)
+
 
 EMBEDDING_MODEL = "intfloat/e5-small-v2"
 embedding_model = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL)
-vector_store = FAISS.from_texts(texts=sentences, embedding=embedding_model)
+vector_store = FAISS.from_texts(texts=chunks, embedding=embedding_model)
 
 query = "What is langchain?"
 
